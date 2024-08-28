9d298c906a81dc1b40041fc3afef73416ff508ce
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-08-28 16:23:33 +0330

    chore: Update Modularity and clean code in exercise-1 API

--- exercises/exercise-1/api/crud.js
+++ exercises/exercise-1/api/crud.js
@@ -1,63 +1,42 @@
-let users = []
-
-const getUser = (user_id) => {
-    user_id = parseInt(user_id);
-    for (let i = 0; i < users.length; i++) {
-        if (users[i]['id'] === user_id) {
-            return users[i];
-        }
-    }
-    return null;
-}
-
-const addUser = (first_name, last_name, age) => {
-    age = parseInt(age);
-    if (age < 0 || age > 120) {
-        return { "error": "User age should be between 0 and 120" }
-    }
-
-    let newUserData = {
-        "id": users.length + 1, first_name, last_name, age
-    }
-    users.push(newUserData);
-    return newUserData;
-}
-
-const deleteUser = (user_id) => {
-    user_id = parseInt(user_id);
-    for (let i = 0; i < users.length; i++) {
-        if (users[i]["id"] === user_id) {
-            // Remove user with specified index from users array
-            users = users.filter((user, index) => index !== i);
-            return true;
-        }
-    }
-    return false;
-}
-
-const updateUser = (user_id, first_name = null, last_name = null, age = null) => {
-    // Update user data with given user_id
-    // Note: passing every user params **except user_id** is not necessary.
-    // Return: If user with given user_id exist, 
-    // user object values will update with given parameters and then return the updated user object. Otherwise return null
-
-    user_id = parseInt(user_id);
-    for (let i = 0; i < users.length; i++) {
-        if (users[i]['id'] === user_id) {
-            let user = users[i];
-            // Set previous values for fields if they are not passed as a given parameter
-            user["first_name"] = first_name ? first_name : user["first_name"];
-            user["last_name"] = last_name ? last_name : user["last_name"];
-            user["age"] = age ? age : user["age"];
-            users[i] = user;
-            return user;
-        }
-    }
-    return null;
-}
-
 let http = require("http");
 let url = require("url");
+
+http.createServer((request, response) => {
+    let urlPath = url.parse(request.url, true);
+
+    switch (request.method) {
+        case "GET":
+            switch (urlPath.pathname) {
+                case "/api/get/":
+                    retrieveUserAPIView(urlPath, response);
+                    break;
+            }
+            break;
+        case "POST":
+            switch (urlPath.pathname) {
+                case "/api/create/":
+                    createUserAPIView(request, response);
+                    break;
+            }
+            break;
+        case "PUT":
+            switch (urlPath.pathname) {
+                case "/api/update/":
+                    updateUserAPIView(request, response);
+                    break;
+            }
+            break;
+        case "DELETE":
+            switch (urlPath.pathname) {
+                case "/api/delete/":
+                    deleteUserAPIView(request, response);
+                    break;
+            }
+            break;
+        default:
+            HTTP404Response(response);
+    }
+}).listen(8000)
 
 const responseMaker = (response, status_code, response_body) => {
     response.setHeader("Content-Type", "application/json");
@@ -142,22 +121,60 @@ const deleteUserAPIView = (request, response) => {
     })
 }
 
-http.createServer((request, response) => {
-    let urlPath = url.parse(request.url, true);
-    switch (urlPath.pathname) {
-        case "/api/get/":
-            retrieveUserAPIView(urlPath, response);
-            break;
-        case "/api/create/" && request.method === "POST":
-            createUserAPIView(request, response);
-            break;
-        case "/api/update/" && request.method === "PUT":
-            updateUserAPIView(request, response);
-            break;
-        case "/api/delete/" && request.method === "DELETE":
-            deleteUserAPIView(request, response);
-            break;
-        default:
-            HTTP404Response(response);
+let users = []
+
+const getUser = (user_id) => {
+    user_id = parseInt(user_id);
+    for (let i = 0; i < users.length; i++) {
+        if (users[i]['id'] === user_id) {
+            return users[i];
+        }
     }
-}).listen(8000)
+    return null;
+}
+
+const addUser = (first_name, last_name, age) => {
+    age = parseInt(age);
+    if (age < 0 || age > 120) {
+        return { "error": "User age should be between 0 and 120" }
+    }
+
+    let newUserData = {
+        "id": users.length + 1, first_name, last_name, age
+    }
+    users.push(newUserData);
+    return newUserData;
+}
+
+const deleteUser = (user_id) => {
+    user_id = parseInt(user_id);
+    for (let i = 0; i < users.length; i++) {
+        if (users[i]["id"] === user_id) {
+            // Remove user with specified index from users array
+            users = users.filter((user, index) => index !== i);
+            return true;
+        }
+    }
+    return false;
+}
+
+const updateUser = (user_id, first_name = null, last_name = null, age = null) => {
+    // Update user data with given user_id
+    // Note: passing every user params **except user_id** is not necessary.
+    // Return: If user with given user_id exist, 
+    // user object values will update with given parameters and then return the updated user object. Otherwise return null
+
+    user_id = parseInt(user_id);
+    for (let i = 0; i < users.length; i++) {
+        if (users[i]['id'] === user_id) {
+            let user = users[i];
+            // Set previous values for fields if they are not passed as a given parameter
+            user["first_name"] = first_name ? first_name : user["first_name"];
+            user["last_name"] = last_name ? last_name : user["last_name"];
+            user["age"] = age ? age : user["age"];
+            users[i] = user;
+            return user;
+        }
+    }
+    return null;
+}
