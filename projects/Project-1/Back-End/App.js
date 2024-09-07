c3df7d51131a3484ab65f2a9872252fc13a91f72
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-09-07 12:47:17 +0330

    feat: Initialize Back-End for project 1

---
+++ projects/Project-1/Back-End/App.js
@@ -0,0 +1,9 @@
+const fastify = require("fastify")({ logger: true });
+
+
+fastify.listen({ port: 8000 }, (err, address) => {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+})
