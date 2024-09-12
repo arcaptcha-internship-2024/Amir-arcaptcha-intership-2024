2f82fa4e6f0c0adc8ebae86d24537f7203a60d8b
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-09-12 18:29:34 +0330

    fix: Fix a bug to expose a port in docker

--- projects/Project-1/Back-End/App.js
+++ projects/Project-1/Back-End/App.js
@@ -1,7 +1,7 @@
 const fastify = require("fastify")({ logger: true });
 fastify.register(require("./routes/users/routes"), { prefix: "api/users/" });
 
-fastify.listen({ port: 8000 }, (err, address) => {
+fastify.listen({ host: "0.0.0.0", port: 8000 }, (err, address) => {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
