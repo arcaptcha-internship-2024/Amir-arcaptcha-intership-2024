c147f8b89f7921c767e7a711c93cfd4debbc532a
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-09-15 15:26:10 +0330

    feat: Add packages for jwt and authentication for user

--- projects/Project-1/Back-End/App.js
+++ projects/Project-1/Back-End/App.js
@@ -1,6 +1,9 @@
 const fastify = require("fastify")({ logger: true });
 fastify.register(require("./routes/contactRequest/routes"), { prefix: "api/contact/" });
 fastify.register(require("./routes/admin/routes"), { prefix: "api/admin/" });
+fastify.register(require("fastify-jwt"), {
+    secret: "supersecret"
+})
 
 fastify.listen({ host: "0.0.0.0", port: 8000 }, (err, address) => {
     if (err) {
