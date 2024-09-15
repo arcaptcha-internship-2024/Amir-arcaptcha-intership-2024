0d749763c178093a8b7d978b59c8a36036942bfd
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-09-15 16:02:33 +0330

    feat: Add fastify jwt for login endpoint

--- projects/Project-1/Back-End/controllers/admin/controllers.js
+++ projects/Project-1/Back-End/controllers/admin/controllers.js
@@ -1,13 +1,14 @@
 const { db } = require("../../ORM/main");
-const { authenticate } = require("../../utils/admin/authentication")
+const { authenticate } = require("../../utils/admin/authentication");
 
 const adminLoginController = async (request, response) => {
     const { username, password } = request.body;
     const user = await db.admin.get(username);
     if (await authenticate(user, password)) {
-        return response.send({ login: true });
+        const token = request.fastify.jwt.sign({ id: user.id, username: username });
+        return response.send({ token });
     }
-    return response.code(404).send({ login: false });
+    return response.code(401).send({ error: "Invalid Username or Password" });
 }
 
 module.exports = {
