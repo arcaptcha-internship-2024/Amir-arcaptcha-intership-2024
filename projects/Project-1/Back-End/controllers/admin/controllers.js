bdfba3d650b149b154149d8f6af7f84d82b009d4
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-09-15 15:45:39 +0330

    feat: Add bcrypt for hash user password for save in database

--- projects/Project-1/Back-End/controllers/admin/controllers.js
+++ projects/Project-1/Back-End/controllers/admin/controllers.js
@@ -1,9 +1,10 @@
 const { db } = require("../../ORM/main");
+const { authenticate } = require("../../utils/admin/authentication")
 
 const adminLoginController = async (request, response) => {
     const { username, password } = request.body;
-    const user = await db.admin.get(username, password);
-    if (user) {
+    const user = await db.admin.get(username);
+    if (await authenticate(user, password)) {
         return response.send({ login: true });
     }
     return response.code(404).send({ login: false });
