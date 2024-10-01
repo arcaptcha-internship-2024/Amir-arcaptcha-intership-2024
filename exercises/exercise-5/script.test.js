b6147ef11bac6404abe0b39419ab3aafe1daf60f
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-01 16:15:03 +0330

    fix: fix a proper test matcher for testing two arrays

--- exercises/exercise-5/script.test.js
+++ exercises/exercise-5/script.test.js
@@ -5,10 +5,11 @@ test("Check remove element from list", () => {
     let array2 = [3];
     let result = arrayDiff(array1, array2);
     expect(result.includes(3)).toBe(false);
+    expect(result).toEqual([1, 2, 4, 5]);
     array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     array2 = [3, 4, 6, 8];
     result = arrayDiff(array1, array2);
-    expect(array2.some(num => result.includes(num))).toBe(false);
+    expect(result).toEqual([1, 2, 5, 7, 9, 10]);
 })
 
 test("Check correct sequence after changes", () => {
