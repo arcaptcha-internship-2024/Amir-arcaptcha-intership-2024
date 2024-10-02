652b3acca4010e38ae667af8fe3200efd061b7cb
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-02 11:34:08 +0330

    tests: change test functionality and update test cases

--- exercises/exercise-5/script.test.js
+++ exercises/exercise-5/script.test.js
@@ -1,22 +1,21 @@
 const { arrayDiff } = require("./script");
 
-test("Check remove element from list", () => {
-    let array1 = [1, 2, 3, 4, 5];
-    let array2 = [3];
-    let result = arrayDiff(array1, array2);
-    expect(result.includes(3)).toBe(false);
-    expect(result).toEqual([1, 2, 4, 5]);
-    array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    array2 = [3, 4, 6, 8];
-    result = arrayDiff(array1, array2);
-    expect(result).toEqual([1, 2, 5, 7, 9, 10]);
-})
+describe("Unit tests for arrayDiff method", () => {
+    let array1, array2, expectedResult, result;
+    beforeAll(() => {
+        array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        array2 = [3, 4, 6, 8];
+        result = arrayDiff(array1, array2);
+        expectedResult = [1, 2, 5, 7, 9, 10];
+    })
+    test("Check remove element from list", () => {
+        expect(result.includes(3)).toBe(false);
+        expect(result.includes(4)).toBe(false);
+        expect(result.includes(6)).toBe(false);
+        expect(result.includes(8)).toBe(false);
+    })
 
-test("Check correct sequence after changes", () => {
-    let array1 = [1, 2, 3, 4, 5];
-    let array2 = [3];
-    expect(arrayDiff(array1, array2).join(", ")).toBe("1, 2, 4, 5");
-    array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    array2 = [3, 4, 6, 8];
-    expect(arrayDiff(array1, array2).join(", ")).toBe("1, 2, 5, 7, 9, 10");
+    test("Check correct sequence after changes", () => {
+        expect(result).toEqual(expectedResult);
+    })
 })
