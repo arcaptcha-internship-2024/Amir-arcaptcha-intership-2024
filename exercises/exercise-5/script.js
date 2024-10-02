28a5a2ea5d3bd77d5abb2dcd04b28b52ba239a28
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-02 11:24:44 +0330

    feat: change function implementation

--- exercises/exercise-5/script.js
+++ exercises/exercise-5/script.js
@@ -1,9 +1,5 @@
 const arrayDiff = (array1, array2) => {
-    
-    array2.forEach(number2 => {
-        array1 = array1.filter(number1 => number1 !== number2)
-    });
-    return array1
+    return array1.filter(number => !array2.includes(number))
 }
 
 module.exports = {
