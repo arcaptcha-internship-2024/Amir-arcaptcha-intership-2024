e488c588634a7dfc17f1afeb698f8017c8d37709
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-07 11:37:39 +0330

    feat: add exception handing for validateCaptcha method

--- projects/Project-1/Back-End/utils/captcha/arcaptcha.js
+++ projects/Project-1/Back-End/utils/captcha/arcaptcha.js
@@ -4,14 +4,17 @@ const axios = require("axios");
 const { ARCAPTCHA_SITEKEY, ARCAPTCHA_SECRETKEY } = process.env;
 
 const validateArcaptchaToken = async (arcaptcha_token) => {
-    const result = await axios.post("https://api.arcaptcha.co/arcaptcha/api/verify", {
-        challenge_id: arcaptcha_token,
-        site_key: ARCAPTCHA_SITEKEY,
-        secret_key: ARCAPTCHA_SECRETKEY
-    })
-    // try, catch here. 
-    const { success } = result.data;
-    return success;
+    try {
+        const result = await axios.post("https://api.arcaptcha.co/arcaptcha/api/verify", {
+            challenge_id: arcaptcha_token,
+            site_key: ARCAPTCHA_SITEKEY,
+            secret_key: ARCAPTCHA_SECRETKEY
+        })
+        const { success } = result.data;
+        return success;
+    } catch (e) {
+        return false;
+    }
 }
 
 module.exports = {
