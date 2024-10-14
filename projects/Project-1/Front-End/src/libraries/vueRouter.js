3ec4e6b3c1832c338a50185325ef25364dc7cf09
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-14 16:04:34 +0330

    feat: make header render dynamicly

--- projects/Project-1/Front-End/src/libraries/vueRouter.js
+++ projects/Project-1/Front-End/src/libraries/vueRouter.js
@@ -1,11 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import ContactUs from "@/pages/contactUs.vue";
-import AdminLogin from "@/admin/Login.vue"
+import AdminLogin from "@/admin/Login.vue";
+import AdminPanel from "@/admin/Panel.vue";
 
 const routes = [
-    { path: "/contactUs", component: ContactUs },
-    { path: "/admin/login", component: AdminLogin },
+    { path: "/contactUs/", component: ContactUs, meta: { showHeader: true } },
+    { path: "/admin/login/", component: AdminLogin, meta: { showHeader: false } },
+    { path: "/admin/", component: AdminPanel, meta: { showHeader: false } },
 ]
 
 const router = createRouter({
