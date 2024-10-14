6600c30e6d19eacb098c061438db62e34c24ddeb
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-14 12:04:05 +0330

    feat: change file name and add captcha validation in admin

--- projects/Project-1/Back-End/controllers/admin/controllers.js
+++ projects/Project-1/Back-End/controllers/admin/controllers.js
@@ -1,8 +1,12 @@
-const { db } = require("../../ORM/main");
-const { authenticate } = require("../../utils/admin/authentication");
-
+const { db } = require(process.cwd() + "/ORM/main");
+const { authenticate } = require(process.cwd() + "/utils/admin/authentication");
+const { validateCaptchaToken } = require(process.cwd() + "/utils/captcha/main")
 const adminLoginController = async (request, response) => {
-    const { username, password } = request.body;
+    const { username, password, arcaptcha_token } = request.body;
+    let isCaptchaValid = await validateCaptchaToken(arcaptcha_token);
+    if (!isCaptchaValid) {
+        return response.code(400).send({ error: "Captcha is not valid" });
+    };
     const user = await db.admin.get(username);
     if (await authenticate(user, password)) {
         const token = request.fastify.jwt.sign({ id: user.id, username: username });
--- projects/Project-1/Back-End/controllers/contactRequest/controllers.js
+++ projects/Project-1/Back-End/controllers/contactRequest/controllers.js
@@ -1,9 +1,9 @@
 const { db } = require("../../ORM/main");
-const { validateArcaptchaToken } = require(process.cwd() + "/utils/captcha/arcaptcha.js");
+const { validateCaptchaToken } = require(process.cwd() + "/utils/captcha/main.js");
 
 const createRequestController = async (request, response) => {
     let { first_name, last_name, phone_number, company_name, job_position, description, arcaptcha_token } = request.body;
-    let isCaptchaValid = await validateArcaptchaToken(arcaptcha_token)
+    let isCaptchaValid = await validateCaptchaToken(arcaptcha_token)
     if (!isCaptchaValid) {
         return response.code(400).send({ error: "Captcha is not valid" });
     };
