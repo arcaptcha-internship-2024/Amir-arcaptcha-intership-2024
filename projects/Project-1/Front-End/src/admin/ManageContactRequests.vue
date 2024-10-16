9e4d77ad27a811b439ef9b2dcad5a477c57c1048
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-16 09:08:12 +0330

    feat: add name for routes, add new route and component for managing contact requests

---
+++ projects/Project-1/Front-End/src/admin/ManageContactRequests.vue
@@ -0,0 +1,15 @@
+<script setup>
+import "@/assets/admin/css/admin-panel.css";
+import MainContentTitle from "./components/MainContentTitle.vue";
+import PanelSidebar from "./components/PanelSidebar.vue";
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <PanelSidebar />
+        <div class="main-content">
+            <MainContentTitle title="Contact Requests" />
+        </div>
+    </div>
+</template>
