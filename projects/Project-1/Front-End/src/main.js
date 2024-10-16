506bc99a15ca2d7eeb25544d27566915fc986e75
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-16 13:06:20 +0330

    feat: add new library for notification sending

--- projects/Project-1/Front-End/src/main.js
+++ projects/Project-1/Front-End/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './libraries/vueRouter';
-
+import Vue3Toastify from 'vue3-toastify';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import 'vue3-toastify/dist/index.css';
 
-createApp(App).use(router).mount('#app');
+
+
+createApp(App).use(router).use(Vue3Toastify, {
+    autoClose: 2000,
+}).mount('#app');
