239077cc15af699ebc08b5d896d8b52fce45ac84
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-16 08:41:25 +0330

    feat: remove Header component from App

--- projects/Project-1/Front-End/src/App.vue
+++ projects/Project-1/Front-End/src/App.vue
@@ -1,13 +1,4 @@
-<script setup>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-import Header from '@/components/Header.vue';
-const route = useRoute();
-const showHeaderStatus = computed(() => route.meta.showHeader)
-</script>
-
 <template>
-  <Header v-if="showHeaderStatus"></Header>
   <main>
     <RouterView />
   </main>
