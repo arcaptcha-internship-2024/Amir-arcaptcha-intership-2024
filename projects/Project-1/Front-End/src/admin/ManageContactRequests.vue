96d874b3109360a1ff60fabb800db9569a3bea5e
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-16 13:47:14 +0330

    feat: add row for table in contact request

--- projects/Project-1/Front-End/src/admin/ManageContactRequests.vue
+++ projects/Project-1/Front-End/src/admin/ManageContactRequests.vue
@@ -2,7 +2,31 @@
 import "@/assets/admin/css/admin-panel.css";
 import MainContentTitle from "./components/MainContentTitle.vue";
 import PanelSidebar from "./components/PanelSidebar.vue";
+import ContactRequestRow from "./components/ContactRequestRow.vue";
+import axios from "axios";
+import { onMounted, ref } from "vue";
+const contactRequests = ref([]);
 
+const getContactRequests = async () => {
+    await axios.get("http://localhost:8000/api/contact/all/", {
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("token")
+        }
+    }).then(({ data }) => {
+        contactRequests.value = data;
+    }).catch(error => {
+        if (error.status === 401) {
+            Swal.fire({
+                icon: "error",
+                title: "Authentication Failed",
+                text: "You're not allowed to access this page"
+            })
+        }
+    })
+}
+onMounted(async () => {
+    await getContactRequests();
+})
 </script>
 
 <template>
@@ -10,6 +34,16 @@ import PanelSidebar from "./components/PanelSidebar.vue";
         <PanelSidebar />
         <div class="main-content">
             <MainContentTitle title="Contact Requests" />
+            <div class="container">
+                <ContactRequestRow 
+                v-for="data in contactRequests"
+                :number=1 
+                :first_name=data.first_name 
+                :last_name=data.last_name 
+                date="2024/12/1" 
+                :status=false
+                />
+            </div>
         </div>
     </div>
 </template>
