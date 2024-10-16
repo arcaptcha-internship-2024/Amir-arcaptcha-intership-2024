b3c1eac24a53ad43632f8cba34aa4d39322512da
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-16 16:55:10 +0330

    feat: add filtering for contactRequests

--- projects/Project-1/Front-End/src/admin/ManageContactRequests.vue
+++ projects/Project-1/Front-End/src/admin/ManageContactRequests.vue
@@ -1,11 +1,17 @@
 <script setup>
-import "@/assets/admin/css/admin-panel.css";
 import MainContentTitle from "./components/MainContentTitle.vue";
-import PanelSidebar from "./components/PanelSidebar.vue";
 import ContactRequestRow from "./components/ContactRequestRow.vue";
+import FilterContactRequests from "./components/FilterContactRequests.vue";
 import axios from "axios";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import "@/assets/admin/css/admin-panel.css";
 const contactRequests = ref([]);
+const displayedResults = ref([]);
+const orderingResult = ref("");
+const filteringResult = ref("");
+const router = useRouter();
+const route = useRoute;
 
 const getContactRequests = async () => {
     await axios.get("http://localhost:8000/api/contact/all/", {
@@ -14,6 +20,7 @@ const getContactRequests = async () => {
         }
     }).then(({ data }) => {
         contactRequests.value = data;
+        displayedResults.value = data;
     }).catch(error => {
         if (error.status === 401) {
             Swal.fire({
@@ -24,15 +31,40 @@ const getContactRequests = async () => {
         }
     })
 }
+
 onMounted(async () => {
     await getContactRequests();
 })
+const changeData = (query, queryKey) => {
+    const queryObj = {};
+    queryObj[queryKey] = query;
+    router.push({
+        path: route.path,
+        query: queryObj
+    })
+}
+const changeOrdering = (query) => {
+    orderingResult.value = query;
+    changeData(query, "ordering");
+}
+const changeFiltering = (query) => {
+    filteringResult.value = query;
+    changeData(query, "filter");
+    if (query === "checked") {
+        displayedResults.value = contactRequests.value.filter(data => data.checked)
+    } else if (query === "not-checked") {
+        displayedResults.value = contactRequests.value.filter(data => !data.checked)
+    } else {
+        displayedResults.value = contactRequests.value;
+    }
+}
 </script>
 
 <template>
     <MainContentTitle title="Contact Requests" />
     <div class="container">
-        <ContactRequestRow v-for="data in contactRequests" :number=1 :first_name=data.first_name
-            :last_name=data.last_name date="2024/12/1" :status=false />
+        <FilterContactRequests @order-by="changeOrdering" @filter-by="changeFiltering" />
+        <ContactRequestRow v-for="(data, index) in displayedResults" :number="index + 1" :first_name=data.first_name
+            :last_name=data.last_name date="2024/12/1" :status=data.checked :key="index" />
     </div>
 </template>
---
+++ projects/Project-1/Front-End/src/admin/components/FilterContactRequests.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+const checkStatusFilter = ref("");
+const orderByDate = ref("");
+</script>
+
+<template>
+    <form @submit.prevent="" class="row p-2 bg-dark text-light">
+        <h5>
+            Filter results:
+        </h5>
+        <div class="col-12 col-md-6">
+            <input type="text" placeholder="Type something..." class="form-control" />
+        </div>
+        <div class="col-6 col-md-3">
+            <select class="form-select" name="check-status" v-model="checkStatusFilter"
+                @change="$emit('filterBy', checkStatusFilter)">
+                <option disabled value="">Check status</option>
+                <option value="all">All</option>
+                <option value="checked">Checked</option>
+                <option value="not-checked">Not Checked</option>
+            </select>
+        </div>
+        <div class="col-6 col-md-3">
+            <select class="form-select" name="check-status" v-model="orderByDate"
+                @change="$emit('orderBy', orderByDate)">
+                <option disabled value="">Ordering</option>
+                <option value="newest">Newest first</option>
+                <option value="oldest">Oldest first</option>
+            </select>
+        </div>
+    </form>
+</template>
