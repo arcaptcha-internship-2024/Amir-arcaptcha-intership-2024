ffdddb452dd306abd73b74b01806030ac8ba21cd
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-16 13:47:14 +0330

    feat: add row for table in contact request

---
+++ projects/Project-1/Front-End/src/admin/components/ContactRequestRow.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['number', 'first_name', 'last_name', 'date', 'status']);
+</script>
+<template>
+    <div class="row bg-secondary p-2 rounded text-white align-items-center my-1">
+        <div class="col-1">{{ number }}</div>
+        <div class="col-4">{{ first_name + " " + last_name }}</div>
+        <div class="col-3">{{ date }}</div>
+        <div class="col-2">
+            <button class="btn btn-success" v-if="status">Checked</button>
+            <button class="btn btn-danger" v-else>Not Checked</button>
+        </div>
+        <div class="col-2">
+            <button class="btn btn-light">View</button>
+        </div>
+    </div>
+</template>
