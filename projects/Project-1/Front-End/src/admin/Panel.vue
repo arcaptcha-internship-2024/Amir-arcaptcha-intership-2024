8d819e1a19dc96dae0c78ba9a0bfec8a879b36c6
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-16 14:08:21 +0330

    feat: change structure of admin panel routes

--- projects/Project-1/Front-End/src/admin/Panel.vue
+++ projects/Project-1/Front-End/src/admin/Panel.vue
@@ -1,47 +1,5 @@
 <script setup>
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import { onMounted, ref } from 'vue';
-import { useRouter } from 'vue-router';
 import PanelSidebar from './components/PanelSidebar.vue';
-import MainContentTitle from './components/MainContentTitle.vue';
-import "@/assets/admin/css/admin-panel.css"
-const contactRequests = ref([]);
-const unreadRequets = ref([]);
-const router = useRouter();
-
-const getContactRequests = async () => {
-    await axios.get("http://localhost:8000/api/contact/all/", {
-        headers: {
-            Authorization: "Bearer " + localStorage.getItem("token")
-        }
-    }).then(({ data }) => {
-        contactRequests.value = data;
-        unreadRequets.value = contactRequests.value.filter(request => request.checked === false);
-    }).catch(error => {
-        if (error.status === 401) {
-            Swal.fire({
-                icon: "error",
-                title: "Authentication Failed",
-                text: "You're not allowed to access this page"
-            })
-
-        }
-        router.push("/admin/login/");
-    })
-}
-
-onMounted(async () => {
-    if (!localStorage.getItem("token")) {
-        Swal.fire({
-            icon: "error",
-            title: "Authentication Failed",
-            text: "You're not allowed to access this page",
-            timer: 2000
-        })
-    }
-    await getContactRequests();
-})
 </script>
 
 <template>
@@ -49,15 +7,7 @@ onMounted(async () => {
     <div class="wrapper">
         <PanelSidebar />
         <div class="main-content">
-            <MainContentTitle title="Dashboard" />
-            <div class="row g-3">
-                <div class="col-md-6">
-                    <div class="content-card bg-success text-light show">
-                        <h3>You have {{ unreadRequets.length }} new requests</h3>
-                        <RouterLink to="" class="btn btn-light">View in detail</RouterLink>
-                    </div>
-                </div>
-            </div>
+            <RouterView />
         </div>
     </div>
 
--- projects/Project-1/Front-End/src/libraries/vueRouter.js
+++ projects/Project-1/Front-End/src/libraries/vueRouter.js
@@ -5,13 +5,20 @@ import ContactUs from "@/pages/contactUs.vue";
 import AdminLogin from "@/admin/Login.vue";
 import AdminPanel from "@/admin/Panel.vue";
 import ManageContactRequests from "@/admin/ManageContactRequests.vue";
+import AdminIndex from "@/admin/components/index.vue";
 
 const routes = [
     { path: "/", component: Home },
     { path: "/contactUs/", component: ContactUs },
     { path: "/admin/login/", component: AdminLogin, name: "adminLogin" },
-    { path: "/admin/contact-requests/", component: ManageContactRequests, name: "adminManageContactRequests" },
-    { path: "/admin/", component: AdminPanel, name: "adminPanel" },
+    { 
+        path: "/admin/", 
+        component: AdminPanel,
+        children: [
+            { path: "", component: AdminIndex, name: "adminPanel" },
+            { path: "contact-requests/", component: ManageContactRequests, name: "adminManageContactRequests" },
+        ] 
+    },
 ]
 
 const router = createRouter({
