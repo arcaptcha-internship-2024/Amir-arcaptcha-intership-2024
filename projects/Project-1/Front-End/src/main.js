f1d466d6897901a39b71debed8b4b081b320ef5a
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-19 12:44:21 +0330

    feat: add pinia for storing datas in global state

--- projects/Project-1/Front-End/src/main.js
+++ projects/Project-1/Front-End/src/main.js
@@ -2,12 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './libraries/vueRouter';
 import Vue3Toastify from 'vue3-toastify';
+import { createPinia } from 'pinia';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import 'vue3-toastify/dist/index.css';
 
+const pinia = createPinia();
+const app = createApp(App);
 
-
-createApp(App).use(router).use(Vue3Toastify, {
-    autoClose: 2000,
-}).mount('#app');
+app.use(router)
+app.use(Vue3Toastify, { autoClose: 2000 });
+app.use(pinia);
+app.mount('#app');
