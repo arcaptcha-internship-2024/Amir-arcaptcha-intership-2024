4a8e2808ad0456dd17d8d4afa8cc5dd8f83faf48
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-21 16:36:25 +0330

    feat: customize signing jwt token in backend

--- projects/Project-1/Back-End/controllers/admin/controllers.js
+++ projects/Project-1/Back-End/controllers/admin/controllers.js
@@ -9,7 +9,7 @@ const adminLoginController = async (request, response) => {
     };
     const user = await db.admin.get(username);
     if (await authenticate(user, password)) {
-        const token = request.fastify.jwt.sign({ id: user.id, username: username });
+        const token = request.fastify.jwt.sign({ id: user.id, username: username, role: user['role'] });
         return response.send({ token });
     }
     return response.code(401).send({ error: "Invalid Username or Password" });
