54c55ee932a27e4c201ed112b3554a9fbea9d511
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-21 13:02:34 +0330

    feat: add alert store to the admin panel for display messages

--- projects/Project-1/Front-End/src/admin/Panel.vue
+++ projects/Project-1/Front-End/src/admin/Panel.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useAlertStore } from '@/store/alerts';
 import PanelSidebar from './components/PanelSidebar.vue';
 import LoginRequired from "@/utils/admin/LoginRequired.vue";
+const alertStore = useAlertStore();
+
+onMounted(() => {
+    alertStore.$fire();
+})
 </script>
 
 <template>
