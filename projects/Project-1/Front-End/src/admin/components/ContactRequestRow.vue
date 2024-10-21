16185baa5f277fe28725c68dfaa602c3778abad3
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-21 14:53:22 +0330

    feat: add a display function to display date correctly in panel

--- projects/Project-1/Front-End/src/admin/components/ContactRequestRow.vue
+++ projects/Project-1/Front-End/src/admin/components/ContactRequestRow.vue
@@ -1,11 +1,15 @@
 <script setup>
 defineProps(['number', 'first_name', 'last_name', 'created_at', 'status']);
+const displayDateFormat = (date) => {
+    const newDate = new Date(date);
+    return `${newDate.getMonth()}/${newDate.getDate()}/${newDate.getFullYear()}`;
+}
 </script>
 <template>
     <div class="row bg-secondary p-2 rounded text-white align-items-center my-1">
         <div class="col-1">{{ number }}</div>
         <div class="col-4">{{ first_name + " " + last_name }}</div>
-        <div class="col-3">{{ created_at }}</div>
+        <div class="col-3">{{ displayDateFormat(created_at) }}</div>
         <div class="col-2">
             <button class="btn btn-success" v-if="status">Checked</button>
             <button class="btn btn-danger" v-else>Not Checked</button>
