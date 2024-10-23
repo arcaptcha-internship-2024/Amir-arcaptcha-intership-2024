1f768e93ef9b6a3a84aad3ab5e0f1dfc49b8ed5b
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-23 15:21:04 +0330

    feat: update vite config and set proxy for backend url

--- projects/Project-1/Front-End/vite.config.js
+++ projects/Project-1/Front-End/vite.config.js
@@ -3,11 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
