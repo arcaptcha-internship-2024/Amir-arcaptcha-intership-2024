0c33184aa07526445315b7ecae011c424712a502
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-24 14:55:03 +0330

    feat: complete admins result display for admin users

---
+++ projects/Project-1/Front-End/src/admin/components/FilterAdmins.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+const usernameFilter = ref("");
+const roleFilter = ref("");
+</script>
+
+<template>
+    <form @submit.prevent="" class="row p-2 bg-dark text-light">
+        <h5>
+            Filter results:
+        </h5>
+        <div class="col-12 col-md-6">
+            <input type="text" placeholder="Type username..." class="form-control" v-model="usernameFilter"
+                @input="$emit('filterUsername', usernameFilter)" />
+        </div>
+        <div class="col-6 col-md-3">
+            <select class="form-select" name="select-role" v-model="roleFilter"
+                @change="$emit('filterRole', roleFilter)">
+                <option disabled value="">Role Filter</option>
+                <option value="all">All</option>
+                <option value="superuser">Superusers</option>
+                <option value="sale-manager">Sale Managers</option>
+            </select>
+        </div>
+    </form>
+</template>
