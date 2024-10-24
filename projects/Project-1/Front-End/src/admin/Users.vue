850c2255433f4eddc0dcf6e2c73640a31ecae96f
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-24 14:55:03 +0330

    feat: complete admins result display for admin users

--- projects/Project-1/Front-End/src/admin/Users.vue
+++ projects/Project-1/Front-End/src/admin/Users.vue
@@ -1,35 +1,42 @@
 <script setup>
-import axios from 'axios';
-import { logoutUser } from '@/utils/admin/authentication';
-import { onMounted, ref } from 'vue';
-import { useRouter } from 'vue-router';
-const router = useRouter();
-const users = ref([]);
+import { useAdminStore } from '@/store/admins';
+import { onMounted, ref, computed } from 'vue';
+import FilterAdmins from './components/FilterAdmins.vue';
+import MainContentTitle from './components/MainContentTitle.vue';
+const adminStore = useAdminStore();
+const filterUsersRole = ref("all");
+const filterUsersName = ref("");
 
-const fetchUsers = () => {
-    axios.get("/api/admin/all/", {
-        withCredentials: true,
-    }).then(({ data }) => {
-        users.value = data;
-    }).catch(error => {
-        if (error.status === 401) {
-            logoutUser();
-            router.push({ name: "adminLogin" });
-        }
-        console.log(error);
+const results = computed(() => {
+    return adminStore.users.filter((user) => {
+        const matchNames = filterUsersName.value === "" || user.username.toLowerCase().includes(filterUsersName.value.toLowerCase());
+        const matchRole = filterUsersRole.value === "all" || user.role === filterUsersRole.value;
+        return matchNames && matchRole;
     })
-}
-onMounted(() => {
-    fetchUsers();
+})
+
+onMounted(async () => {
+    await adminStore.fetch();
+    results = adminStore.users;
 })
 </script>
 
 <template>
-    <div class="row bg-secondary p-2 rounded text-white align-items-center my-1" v-for="user in users">
-        <div class="col-1">{{ user.id }}</div>
-        <div class="col-4">{{ user.username }}</div>
-        <div class="col-2">
-            <button class="btn btn-light">View</button>
+    <MainContentTitle title="Admin Management" />
+    <div class="container">
+        <FilterAdmins @filterRole="value => filterUsersRole = value"
+            @filterUsername="value => filterUsersName = value" />
+        <div class="row bg-dark p-2 rounded text-white align-items-center my-1" v-for="(user, index) in results" :key="index" >
+            <div class="col-1">{{ index + 1 }}</div>
+            <div class="col-4">{{ user.username }}</div>
+            <div class="col-4">
+                <button type="button" class="btn btn-primary" v-if="user.role === 'superuser'">Admin</button>
+                <button type="button" class="btn btn-secondary" v-if="user.role === 'sale-manager'">Sale
+                    Manager</button>
+            </div>
+            <div class="col-2">
+                <button class="btn btn-light">View</button>
+            </div>
         </div>
     </div>
 </template>
