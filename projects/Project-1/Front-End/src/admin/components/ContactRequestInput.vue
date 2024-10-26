444ee8b725ec050b250ecd094bfbf290df5e362c
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-10-26 15:55:29 +0330

    feat: emit input data from form inputs components

--- projects/Project-1/Front-End/src/admin/components/ContactRequestInput.vue
+++ projects/Project-1/Front-End/src/admin/components/ContactRequestInput.vue
@@ -1,11 +1,14 @@
 <script setup>
-defineProps(["input_label", "input_id", "input_value"])
+defineProps(["input_label", "input_id", "input_value"]);
+import { ref } from 'vue';
+const inputData = ref("");
 </script>
 <template>
     <div class="col-12 my-2">
         <label :for="input_id" class="mb-1">
             {{ input_label }}:
         </label>
-        <input type="text" class="form-control" :id="input_id" :value="input_value" />
+        <input type="text" class="form-control" :id="input_id" :value="input_value" v-model="inputData"
+            @input="$emit('input', inputData)" />
     </div>
 </template>
--- projects/Project-1/Front-End/src/admin/components/ContactRequestStatus.vue
+++ projects/Project-1/Front-End/src/admin/components/ContactRequestStatus.vue
@@ -1,12 +1,14 @@
 <script setup>
-defineProps(["input_label", "input_id", "input_value"])
+defineProps(["input_label", "input_id", "input_value"]);
+import { ref } from 'vue';
+const inputData = ref("");
 </script>
 <template>
     <div class="col-12 my-2">
         <label :for="input_id" class="mb-1">
             {{ input_label }}:
         </label>
-        <select name="" id="" class="form-select">
+        <select name="" id="" class="form-select" v-model="inputData" @input="$emit('input', inputData)">
             <option :selected="input_value === 'not-checked'" value="not-checked">Not checked</option>
             <option :selected="input_value === 'in-progress'" value="in-progress">In progress</option>
             <option :selected="input_value === 'completed'" value="completed">Completed</option>
