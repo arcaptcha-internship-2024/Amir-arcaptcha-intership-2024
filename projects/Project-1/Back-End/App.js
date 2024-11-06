40739058f511ba305f9dc341b427ae9c436a54f8
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-06 17:01:25 +0330

    feat: add config for swagger to get manage by fastify

--- projects/Project-1/Back-End/App.js
+++ projects/Project-1/Back-End/App.js
@@ -1,11 +1,55 @@
 require("dotenv").config();
 const fastify = require("fastify")({ logger: true });
 const cors = require('@fastify/cors');
+
+fastify.register(require('@fastify/swagger'), {
+    swagger: {
+        info: {
+            title: 'API Documentation',
+            description: 'API documentation for your Fastify application',
+            version: '1.0.0'
+        },
+        host: `${process.env.SERVER_HOST || '0.0.0.0'}:${process.env.SERVER_PORT || 8000}`,
+        schemes: ['http', 'https'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        tags: [
+            { name: 'contact', description: 'Contact related endpoints' },
+            { name: 'admin', description: 'Admin related endpoints' }
+        ],
+        securityDefinitions: {
+            apiKey: {
+                type: 'apiKey',
+                name: 'Authorization',
+                in: 'header'
+            }
+        }
+    }
+});
+
+// Register Swagger UI
+fastify.register(require('@fastify/swagger-ui'), {
+    routePrefix: '/doc',
+    uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false
+    },
+    uiHooks: {
+        onRequest: function (request, reply, next) { next() },
+        preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
+});
+
 fastify.register(require("@fastify/cookie"), {
     secret: "supersecret",
     hook: "onRequest",
 
 });
+
 fastify.register(require("./routes/contactRequest/routes"), { prefix: "api/contact/" });
 fastify.register(require("./routes/admin/routes"), { prefix: "api/admin/" });
 fastify.register(require("@fastify/jwt"), {
