2d754ceaadd5180e1a339470fa8b8aa94436f876
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-11 12:46:17 +0330

    feat: update pagination function

--- projects/Project-1/Back-End/utils/pagination/main.js
+++ projects/Project-1/Back-End/utils/pagination/main.js
@@ -4,31 +4,46 @@ const getPaginatedResultForFileORM = (startIndex, endIndex, dataArr) => {
     return dataArr.slice(startIndex, endIndex);
 }
 
+const filterResultsByQuery = (query = "", data = []) => {
+    return data.filter(obj => {
+        return obj.first_name.includes(query) || obj.last_name.includes(query)
+    });
+}
+
+const getPaginationData = (data = [], startIndex, endIndex, currentPage) => {
+    return {
+        next: {
+            has_next: endIndex < data.length,
+            next_page: endIndex < data.length ? currentPage + 1 : 0
+        },
+        previous: {
+            has_previous: startIndex > 0,
+            previous_page: startIndex > 0 ? currentPage - 1 : 0
+        },
+    }
+}
+
 const getPaginatedResult = (page = 1, limit = 5, query = "", data) => {
-    if (typeof page !== "number") {
-        page = 1;
-    }
-
-    if (typeof limit !== "number") {
-        limit = 1;
-    }
-
-
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
-    if (query === "") {
-        if (process.env.DB_TYPE === "file") {
-            return getPaginatedResultForFileORM(startIndex, endIndex, data);
-        }
-    }else {
-        if (process.env.DB_TYPE === "file") {
-            data = data.filter(obj => {
-                return obj.first_name.includes(query) || obj.last_name.includes(query)
-            });
-            return getPaginatedResultForFileORM(startIndex, endIndex, data)
-        }
+    let paginationData = {};
+    let result = [];
+    if (query !== "") {
+        data = filterResultsByQuery(query, data);
+    }
+    paginationData = getPaginationData(data, startIndex, endIndex, page);
+    switch (process.env.DB_TYPE) {
+        case "file":
+            result = getPaginatedResultForFileORM(startIndex, endIndex, data);
+            break
+        default:
+            result = getPaginatedResultForFileORM(startIndex, endIndex, data);
     }
 
+    return {
+        contact_requests: result,
+        ...paginationData
+    }
 }
 
 module.exports = {
