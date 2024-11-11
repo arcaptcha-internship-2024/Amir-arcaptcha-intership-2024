79a6c1011e77941938f35bedde991df52e3249a5
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-11 14:05:44 +0330

    fix: fix some style issues in pagination component

--- projects/Project-1/Front-End/src/admin/components/Pagination.vue
+++ projects/Project-1/Front-End/src/admin/components/Pagination.vue
@@ -5,12 +5,12 @@ defineProps(["pagination_data"]);
 <template>
     <nav aria-label="Page navigation">
         <ul class="pagination">
-            <li class="page-item" v-if="pagination_data.previous.has_previous">
-                <a class="page-link" href="#" aria-label="Previous">
+            <li class="page-item pointer" v-if="pagination_data.previous.has_previous">
+                <span class="page-link" aria-label="Previous" @click="$emit('previous_page', pagination_data.previous.previous_page)">
                     <span aria-hidden="true">&laquo;</span>
-                </a>
+                </span>
             </li>
-            <li class="page-item" v-if="pagination_data.previous.has_previous">
+            <li class="page-item pointer" v-if="pagination_data.previous.has_previous">
                 <span class="page-link" @click="$emit('previous_page', pagination_data.previous.previous_page)">
                     {{ pagination_data.previous.previous_page }}
                 </span>
@@ -20,16 +20,22 @@ defineProps(["pagination_data"]);
                     {{ pagination_data.current_page }}
                 </span>
             </li>
-            <li class="page-item" v-if="pagination_data.next.has_next">
+            <li class="page-item pointer" v-if="pagination_data.next.has_next">
                 <span class="page-link" @click="$emit('next_page', pagination_data.next.next_page)">
                     {{ pagination_data.next.next_page }}
                 </span>
             </li>
-            <li class="page-item" v-if="pagination_data.next.has_next">
-                <a class="page-link" href="#" aria-label="Next">
+            <li class="page-item pointer" v-if="pagination_data.next.has_next">
+                <span class="page-link" aria-label="Next" @click="$emit('next_page', pagination_data.next.next_page)">
                     <span aria-hidden="true">&raquo;</span>
-                </a>
+                </span>
             </li>
         </ul>
     </nav>
 </template>
+
+<style lang="css">
+.pointer {
+    cursor: pointer;
+}
+</style>
