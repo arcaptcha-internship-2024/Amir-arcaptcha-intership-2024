c9354e9bf293c8b67604d6dd6239f3466fb29185
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-11 12:53:15 +0330

    chore: change search contact request object behaviour

--- projects/Project-1/Front-End/src/admin/components/FilterContactRequests.vue
+++ projects/Project-1/Front-End/src/admin/components/FilterContactRequests.vue
@@ -10,9 +10,9 @@ const searchQuery = ref("");
         <h5>
             Filter results:
         </h5>
-        <div class="col-12 col-md-9 my-1 my-md-0">
-            <input type="text" placeholder="Type something..." class="form-control" v-model="searchQuery"
-                @input="$emit('searchBy', searchQuery)" />
+        <div class="col-12 col-md-9 my-1 my-md-0 d-flex flex-row justify-content-center align-items-center">
+            <input type="text" placeholder="Type something..." class="form-control" v-model="searchQuery" />
+            <button class="btn btn-success mx-1" @click="$emit('searchBy', searchQuery)">Search</button>
         </div>
         <div class="col-6 col-md-3 my-1 my-md-0">
             <select class="form-select" name="check-status" v-model="checkStatusFilter"
