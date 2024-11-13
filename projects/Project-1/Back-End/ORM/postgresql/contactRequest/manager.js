ad8b17f5d06e2b0b1a67fb2c5db43305fd87292c
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-13 15:22:57 +0330

    fix: fix an issue in creating object

--- projects/Project-1/Back-End/ORM/postgresql/contactRequest/manager.js
+++ projects/Project-1/Back-End/ORM/postgresql/contactRequest/manager.js
@@ -9,7 +9,7 @@ const createContactRequest = async (first_name, last_name, phone_number, company
                 ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
             [first_name, last_name, phone_number, company_name, job_position, description, status, admin_message]
         );
-        return res[rows][0];
+        return res.rows[0];
     } catch (err) {
         console.log("Failed to create contactRequest obj with error: " + err)
     }
