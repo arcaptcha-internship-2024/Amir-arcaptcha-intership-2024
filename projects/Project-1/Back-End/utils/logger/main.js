63d35d04b9a6f51d73358a690f6ebdfec828ba08
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-16 15:24:32 +0330

    feat: update logger methods in backend

--- projects/Project-1/Back-End/utils/logger/main.js
+++ projects/Project-1/Back-End/utils/logger/main.js
@@ -44,14 +44,40 @@ const consumeDataFromQueue = () => {
     })
 }
 
-const writeLogsInFile = (message) => {
+const getLogMessage = (message) => {
+    return JSON.stringify({
+        message: message,
+        date: new Date().toLocaleDateString().replaceAll("/", "-")
+    })
+}
+
+const getFilePath = () => {
     let path = process.cwd() + "/logs/";
     let fileName = new Date().toLocaleDateString().replaceAll("/", "-") + ".log";
     let filePath = path + fileName;
-    fs.appendFile(filePath, message + "\n", { encoding: "utf8" });
+    return filePath
+}
+
+const writeLogsInFile = (message) => {
+    const filePath = getFilePath();
+    fs.appendFile(filePath, getLogMessage(message) + "\n", { encoding: "utf8" });
+}
+
+const getLastLogMessages = async () => {
+    const filePath = getFilePath();
+    try {
+        let logs = await fs.readFile(filePath, { encoding: "utf8" });
+        logs = logs.split("\n")
+        logs = logs.reverse().filter(log => log !== "").slice(0, 6);
+        logs = logs.map(log => JSON.parse(log));
+        return logs
+    } catch (err) {
+        console.log("Failed to open log file with error: " + err);
+    }
 }
 
 module.exports = {
     sendLogToQueue,
-    consumeDataFromQueue
+    consumeDataFromQueue,
+    getLastLogMessages
 }
