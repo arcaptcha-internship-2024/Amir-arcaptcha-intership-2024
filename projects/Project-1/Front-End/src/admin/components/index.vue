e5bafdd585f5e4b2c20b65fcfcf45b877e887b87
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-16 15:25:27 +0330

    feat: display logs in admin panel

--- projects/Project-1/Front-End/src/admin/components/index.vue
+++ projects/Project-1/Front-End/src/admin/components/index.vue
@@ -1,19 +1,32 @@
 <script setup>
 import MainContentTitle from './MainContentTitle.vue';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import "@/assets/admin/css/admin-panel.css"
 import { useUserStore } from '@/store/user';
 import { useAlertStore } from '@/store/alerts';
 import { useContactRequestStore } from '@/store/contactRequests';
 import { useRouter } from 'vue-router';
 import { logoutUser } from '@/utils/admin/authentication';
+import axios from "axios";
 const userStore = useUserStore();
 const alertStore = useAlertStore();
 const contactRequestStore = useContactRequestStore();
 const router = useRouter();
+const actionLogs = ref([]);
 
 const getContactRequests = async () => {
     await contactRequestStore.$fetch();
+}
+
+const getLogs = async () => {
+    axios.get("http://localhost:8000/api/admin/logs/", { withCredentials: true })
+        .then(({ data }) => {
+            actionLogs.value = data;
+        })
+        .catch(error => {
+            alertStore.setMessage("Failed to load admin actions", "error")
+            alertStore.$fire()
+        })
 }
 
 onMounted(async () => {
@@ -24,6 +37,7 @@ onMounted(async () => {
     }
     alertStore.$fire();
     await getContactRequests();
+    await getLogs();
 })
 </script>
 <template>
@@ -42,6 +56,15 @@ onMounted(async () => {
                 <RouterLink :to="{ name: 'adminManageUsers' }" class="btn btn-light">
                     View in detail
                 </RouterLink>
+            </div>
+        </div>
+        <div class="p-2 rounded rounded-2 bg-dark text-light d-flex flex-column w-100">
+            <h3 class="my-2">Admin Actions:</h3>
+            <div class="bg-secondary p-2 my-1 w-100 rounded-2" v-for="action in actionLogs">
+                {{ action.date }}:
+                <span class="fw-bold">
+                    {{ action.message }}
+                </span>
             </div>
         </div>
     </div>
