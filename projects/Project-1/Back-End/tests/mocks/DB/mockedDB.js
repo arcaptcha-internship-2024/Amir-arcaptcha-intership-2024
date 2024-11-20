a9f4950f5fddd755dcac051fee27e2dd6979546f
amir mahdi kahdouii <amirmahdikahdooi@gmail.com>
2024-11-20 23:37:09 +0330

    tests: updated mocked db

--- projects/Project-1/Back-End/tests/mocks/DB/mockedDB.js
+++ projects/Project-1/Back-End/tests/mocks/DB/mockedDB.js
@@ -1,6 +1,9 @@
 module.exports = {
     admin: {
-
+        get: async (username) => {
+            let validUsername = "admin"
+            return username === validUsername;
+        }
     },
     contactRequest: {
         create: async ({ first_name, last_name, phone_number, company_name, job_position, description }) => ({
